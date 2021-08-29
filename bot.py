ccf2aadd45b6023624c5dfa671f3e189a456fa6a
dev single <89737813+dev-single@users.noreply.github.com>
2021-08-29 23:21:31 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,6 @@ import os
 client = discord.Client()
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.dnd)
-@client.event
-async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     servers = len(client.guilds)
     members = 0
